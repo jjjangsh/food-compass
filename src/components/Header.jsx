362e6562ce1527e4cafbf5772c0d6d15955e9d47
Your Name <you@example.com>
2024-09-13 10:09:07 +0900

    feat:레이아웃간단하게

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
-const Header = () => {
-  return <div>Header</div>;
+import { useNavigate } from "react-router-dom";
+
+const Header = ({ children }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="fixed w-full bg-sky-200 h-20 p-5">
+        <div className="flex flex-row justify-between">
+          <p onClick={() => navigate("/")}>로고입니다</p>
+          <div>
+            <button onClick={() => navigate("/signin")}>로그인</button>
+            <button onClick={() => navigate("/signup")}>회원가입</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full pt-24">{children}</div>
+    </>
+  );
 };
 
 export default Header;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,26 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from '../components/Header';
-import Home from '../pages/Home';
-import SignIn from './../pages/SignIn';
-import SignUp from './../pages/SignUp';
-import PostDetail from './../pages/PostDetail';
-import PostWrite from './../pages/PostWrite';
-import PostUpdate from './../pages/PostUpdate';
-import MyPage from './../pages/MyPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
+import Home from "../pages/Home";
+import SignIn from "./../pages/SignIn";
+import SignUp from "./../pages/SignUp";
+import PostDetail from "./../pages/PostDetail";
+import PostWrite from "./../pages/PostWrite";
+import PostUpdate from "./../pages/PostUpdate";
+import MyPage from "./../pages/MyPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/postdetail" element={<PostDetail />} />
-        <Route path="/postwrite" element={<PostWrite />} />
-        <Route path="/postupdate" element={<PostUpdate />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/postdetail" element={<PostDetail />} />
+          <Route path="/postwrite" element={<PostWrite />} />
+          <Route path="/postupdate" element={<PostUpdate />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </Header>
     </BrowserRouter>
   );
 };
