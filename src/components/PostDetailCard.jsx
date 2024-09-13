34a16517727d3bd999ad6597f8314e94086d1590
heerokj <jungheerok@gmail.com>
2024-09-13 22:09:25 +0900

    feat : 해당 가게 위치 불러오기 성공

--- src/components/PostDetailCard.jsx
+++ src/components/PostDetailCard.jsx
@@ -1,30 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import userStore from '../zustand/userStore';
 
-const PostDetailCard = () => {
+const PostDetailCard = ({ post, postId }) => {
   const navigate = useNavigate();
   const { user } = userStore();
-
-  //포스트 아이디 가져오기
-  const [searchParams, setSearchParams] = useSearchParams();
-  const postId = searchParams.get('id'); // postId = 1
-
-  //해당 포스트 가져와서 보여주기
-  const {
-    data: post,
-    isError: isPostError,
-    isPending: isPostPending
-  } = useQuery({
-    queryKey: ['posts', postId],
-    queryFn: async () => {
-      const response = await axios.get(`http://localhost:4000/posts?id=${postId}`);
-      return response.data[0]; //질문
-    }
-  });
-  if (isPostPending) return <div>포스트를 불러오고 있습니다 ...</div>;
-  if (isPostError) return <div>에러가 발생했습니다 ...</div>;
 
   //포스트 삭제하기
   const deletePost = async () => {
--- src/components/PostDetailMap.jsx
+++ src/components/PostDetailMap.jsx
@@ -1,25 +1,51 @@
+/*global kakao*/
 import { useEffect } from 'react';
 
-const PostDetailMap = () => {
-  const apiKey = import.meta.env.VITE_MAP_API_KEY;
+const PostDetailMap = ({ post }) => {
   useEffect(() => {
-    // 스크립트 태그 생성
-    const mapScript = document.createElement('script');
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`; // autoload=false를 사용해 자동 로드 방지
-    document.head.appendChild(mapScript);
-
     // 스크립트 로드가 완료된 후 실행
-    mapScript.onload = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3
-        };
-        const map = new window.kakao.maps.Map(container, options);
-      });
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
     };
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    console.log('map===>', map);
+
+    var services = kakao.maps.services;
+    if (!services) {
+      console.log('kakao.maps.services 를 찾을 수 없음 :', kakao.maps);
+      return;
+    } else {
+      // 주소-좌표 변환 객체를 생성합니다
+      var geocoder = new kakao.maps.services.Geocoder();
+
+      // 가게주소
+      const address = post.address;
+      // 주소로 좌표를 검색합니다
+      geocoder.addressSearch(address, function (result, status) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+          });
+
+          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          var infowindow = new kakao.maps.InfoWindow({
+            content: `<div style="width:150px;text-align:center;padding:6px 0;">${post.title}</div>`
+          });
+          infowindow.open(map, marker);
+
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          map.setCenter(coords);
+        }
+      });
+    }
   }, []);
 
   return (
